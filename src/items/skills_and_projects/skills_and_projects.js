93617998a0e162a598b72ae79897efa3793b73c0
Lauiss <famann57@gmail.com>
2024-04-26 18:29:50 +0200

    added traduction & begining of theme

--- src/items/skills_and_projects/skills_and_projects.js
+++ src/items/skills_and_projects/skills_and_projects.js
@@ -1,4 +1,5 @@
-import { faAngular, faJs, faReact } from '@fortawesome/free-brands-svg-icons';
+import { faAngular, faJava, faJs, faReact, faSwift, faWindows } from '@fortawesome/free-brands-svg-icons';
+import { faCamera, faCode, faDatabase, faTerminal, faVideo } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import { Chrono } from 'react-chrono';
@@ -10,40 +11,45 @@ const SkillsAndProjects = ({ translate }) => {
     { name: 'JavaScript', icon: faJs },
     { name: 'Angular', icon: faAngular },
     { name: 'React', icon: faReact },
-    { name: 'Ionic', icon: faReact },
-    { name: 'Java', icon: faReact },
-    { name: 'Swift', icon: faReact },
-    { name: 'Batch', icon: faReact },
-    { name: 'Shell', icon: faReact },
-    { name: 'Powershell', icon: faReact },
-    { name: 'SQL', icon: faReact },
-    { name: 'C#', icon: faReact },
-    // Ajoutez plus de compétences ici
+    { name: 'Ionic', icon: faAngular },
+    { name: 'Java', icon: faJava },
+    { name: 'Swift', icon: faSwift },
+    { name: 'Batch', icon: faTerminal },
+    { name: 'Shell', icon: faTerminal },
+    { name: 'Powershell', icon: faTerminal },
+    { name: 'SQL', icon: faDatabase },
+    { name: 'C#', icon: faCode },
   ];
 
   const otherSkills = [
-    { name: 'Montage photo', icon: faJs },
-    { name: 'Montage vidéo', icon: faAngular },
-    { name: 'Suite Adobe', icon: faReact },
-    { name: 'Suite Microsoft', icon: faReact },
-    { name: 'Gestion de projet', icon: faReact },
-    // Ajoutez plus de compétences ici
+    { name: translate('PHOTO_EDITING'), icon: faCamera },
+    { name: translate('VIDEO_EDITING'), icon: faVideo },
+    { name: translate('SUITE_ADOBE'), icon: faReact },
+    { name: translate('SUITE_MICROSOFT'), icon: faWindows },
+    { name: translate('PROJECT_MANAGEMENT'), icon: faReact },
   ];
-
-
 
   const projects = [
-    { title: 'Autoclicker pour Raid Shadow Legends', date: '2020-06-21' },
-    { title: 'Bot discord avec différentes fonctionnalités', date: '2021-01-24' },
-    { title: 'Petit générateur de mot de passe', date: '2021-08-02' },
-    { title: 'API Vinted fonctionnelle avec un manageur de proxy', date: '2023-03-10' },
-    { title: 'Portfolio', date: '2024-02-26' },
+    { title: translate('AUTOCLICK'), date: '2020-06-21' },
+    { title: translate('DISCORD_BOT'), date: '2021-01-24' },
+    { title: translate('PWD_GENERATOR'), date: '2021-08-02' },
+    { title: translate('VINTED_API_AND_PROXY_MANAGER'), date: '2023-03-10' },
+    { title: translate('PORTFOLIO'), date: '2024-02-26' },
   ];
+
+  const theme = {
+    primary: '#63E6BE',
+    secondary: '#FFFFFF',
+    cardBgColor: '#FFFFFF',
+    cardForeColor: '#000000',
+    titleColor: '#000000',
+    titleColorActive: '#63E6BE'
+  }
 
   return (
     <div className="skills-and-projects">
       <div className="skills">
-        <h2>{translate(('SKILLS'))}</h2>
+        <h2>{translate('SKILLS')}</h2>
 
         <div className='skills-container'>
           {skills.map((skill, index) => (
@@ -53,7 +59,7 @@ const SkillsAndProjects = ({ translate }) => {
             </div>
           ))}
         </div>
-        <h2> Autres compétences</h2>
+        <h2>{translate('OTHER_SKILLS')}</h2>
         <div className='other-skills-container'>
           {otherSkills.map((skill, index) => (
             <div key={index} className="other-skill-badge">
@@ -64,8 +70,9 @@ const SkillsAndProjects = ({ translate }) => {
         </div>
       </div>
       <div className="projects">
-        <h2>Projets</h2>
-        <Chrono mode="VERTICAL_ALTERNATING" disableToolbar={true} timelinePointShape='diamond' scrollable={{ scrollbar: true }}
+        <h2>{translate('PROJECTS')}</h2>
+        <Chrono mode="VERTICAL_ALTERNATING" disableToolbar={true} timelinePointShape='diamond'
+          scrollable={{ scrollbar: true }} theme={theme} className='chrono'
           items={projects.map(project => ({ title: project.title, cardTitle: project.date }))} />
       </div>
     </div>
