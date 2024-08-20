154ec091cdd03ee817cc2ba8a233dbfb8d0c7a7e
Lauiss <famann57@gmail.com>
2024-08-20 21:31:24 +0200

    added new project logic

--- src/items/projects/projects.js
+++ src/items/projects/projects.js
@@ -1,11 +1,9 @@
-import React from 'react';
-import { Chrono } from 'react-chrono';
+import React, { useState } from 'react';
+import pfp from '../../assets/pdp.png';
 import './projects.css';
 
 
-const Projects = ({ translate, isDarkMode, language }) => {
-
-  console.log(language);
+const Projects = ({ translate }) => {
 
   const projects = [
     { date: translate('PORTFOLIO_DATE'), title: translate('PORTFOLIO_TITLE'), description: translate('PORTFOLIO_DESCRIPTION'), repoType: translate('PRIVATE_REPOSITORY') },
@@ -16,43 +14,49 @@ const Projects = ({ translate, isDarkMode, language }) => {
     { date: translate('AUTOCLICKER_DATE'), title: translate('AUTOCLICKER_TITLE'), description: translate('AUTOCLICKER_DESCRIPTION'), repoType: 'https://github.com/Lauiss/autorsl' },
   ];
 
-  const theme = isDarkMode ? {
-    primary: '#63E6BE',
-    secondary: '#282c34',
-    cardBgColor: '#272829',
-    cardForeColor: '#f7f7f7',
-    titleColor: '#f7f7f7',
-    titleColorActive: '#63E6BE',
-    cardSubtitleColor: '#f7f7f7',
-    cardTitleColor: '#63E6BE',
-    cardDetailsColor: '#f7f7f7'
+ const [currentIndex, setCurrentIndex] = useState(0);
 
-  } : {
-    primary: '#63E6BE',
-    secondary: '#f7f7f7',
-    cardBgColor: '#f8f8f8',
-    cardForeColor: '#f7f7f7',
-    titleColor: '#282c34',
-    titleColorActive: '#282c34',
-    cardSubtitleColor: '#282c34',
-    cardTitleColor: '#63E6BE',
-    cardDetailsColor: '#282c34'
-  }
+  const handlePrevClick = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? projects.length - 1 : prevIndex - 1
+    );
+  };
+
+  const handleNextClick = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === projects.length - 1 ? 0 : prevIndex + 1
+    );
+  };
+
+  const project = projects[currentIndex];
 
   return (
-    <div className="projects">
+    <div className="container">
       <h2>{translate('PERSONAL_PROJECTS')}</h2>
-      <div className='chrono'>
-        <Chrono
-          key={`${language}-${isDarkMode} ? 'dark' : 'light'`}
-          mode="VERTICAL_ALTERNATING"
-          disableToolbar={true}
-          timelinePointShape='diamond'
-          scrollable={{ scrollbar: true }}
-          theme={theme}
-          className='chrono'
-          items={projects.map(project => ({ title: project?.date, cardTitle: project?.title, cardSubtitle: project?.repoType, cardDetailedText: project?.description }))} />
+    <div className="projects">
+      <div className="carousel-container">
+        <button className="carousel-button left" onClick={handlePrevClick}>
+          &#9664;
+        </button>
+        <div className="carousel">
+          <div className="card-container">
+            <a className='card-link' href={project?.repoType}>
+              <div className='card-background'>
+                <img className='card-image' src={pfp} alt={project?.title} />
+                <div>
+                  <h3 className="card-title">{project?.title}</h3>
+                  <p className="card-subtitle">{project?.date}</p>
+                  <p className='card-text'>{project?.description}</p>
+                </div>
+              </div>
+            </a>
+          </div>
+        </div>
+        <button className="carousel-button right" onClick={handleNextClick}>
+          &#9654;
+        </button>
       </div>
+    </div>
     </div>
   );
 };
