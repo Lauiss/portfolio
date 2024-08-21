db50e509c5eff024393872812f8572fce1d3d667
Lauiss <famann57@gmail.com>
2024-08-21 15:26:24 +0200

    added last changes

--- src/items/projects/projects.js
+++ src/items/projects/projects.js
@@ -1,20 +1,24 @@
 import React, { useState } from 'react';
-import pfp from '../../assets/pdp.png';
+import clicklogo from '../../assets/click_logo.jpg';
+import codelogo from '../../assets/code_logo.png';
+import discordlogo from '../../assets/discord_logo.jpg';
+import wildjamlogo from '../../assets/godot_wildjam_logo.jpg';
+import passwordgeneratorlogo from '../../assets/password_generator_logo.jpg';
 import './projects.css';
 
-
 const Projects = ({ translate }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
 
   const projects = [
+    { date: translate('GODOT_WILDJAM_70_DATE'), title: translate('GODOT_WILDJAM_70_TITLE'), description: translate('GODOT_WILDJAM_70_DESCRIPTION'), repoType: 'https://lauiss.itch.io/dawnfire', image: wildjamlogo },
+    { date: translate('GODOT_WILDJAM_71_DATE'), title: translate('GODOT_WILDJAM_71_TITLE'), description: translate('GODOT_WILDJAM_71_DESCRIPTION'), repoType: 'https://graphific.itch.io/colorbeat', image: wildjamlogo },
     { date: translate('PORTFOLIO_DATE'), title: translate('PORTFOLIO_TITLE'), description: translate('PORTFOLIO_DESCRIPTION'), repoType: translate('PRIVATE_REPOSITORY') },
     { date: translate('OPDL_DATE'), title: translate('OPDL_TITLE'), description: translate('OPDL_DESCRIPTION'), repoType: 'https://github.com/Lauiss/onepiecedle' },
     { date: translate('VINTED_API_AND_PROXY_MANAGER_DATE'), title: translate('VINTED_API_AND_PROXY_MANAGER_TITLE'), description: translate('VINTED_API_AND_PROXY_MANAGER_DESCRIPTION'), repoType: 'https://github.com/Lauiss/vinApi' },
-    { date: translate('PWD_GENERATOR_DATE'), title: translate('PWD_GENERATOR_TITLE'), description: translate('PWD_GENERATOR_DESCRIPTION'), repoType: 'https://github.com/Lauiss/mdpgenerator' },
-    { date: translate('DISCORD_BOT_DATE'), title: translate('DISCORD_BOT_TITLE'), description: translate('DISCORD_BOT_DESCRIPTION'), repoType: translate('PRIVATE_REPOSITORY') },
-    { date: translate('AUTOCLICKER_DATE'), title: translate('AUTOCLICKER_TITLE'), description: translate('AUTOCLICKER_DESCRIPTION'), repoType: 'https://github.com/Lauiss/autorsl' },
+    { date: translate('PWD_GENERATOR_DATE'), title: translate('PWD_GENERATOR_TITLE'), description: translate('PWD_GENERATOR_DESCRIPTION'), repoType: 'https://github.com/Lauiss/mdpgenerator', image: passwordgeneratorlogo },
+    { date: translate('DISCORD_BOT_DATE'), title: translate('DISCORD_BOT_TITLE'), description: translate('DISCORD_BOT_DESCRIPTION'), repoType: translate('PRIVATE_REPOSITORY'), image: discordlogo },
+    { date: translate('AUTOCLICKER_DATE'), title: translate('AUTOCLICKER_TITLE'), description: translate('AUTOCLICKER_DESCRIPTION'), repoType: 'https://github.com/Lauiss/autorsl', image: clicklogo }
   ];
-
- const [currentIndex, setCurrentIndex] = useState(0);
 
   const handlePrevClick = () => {
     setCurrentIndex((prevIndex) =>
@@ -33,16 +37,15 @@ const Projects = ({ translate }) => {
   return (
     <div className="container">
       <h2>{translate('PERSONAL_PROJECTS')}</h2>
-    <div className="projects">
-      <div className="carousel-container">
-        <button className="carousel-button left" onClick={handlePrevClick}>
-          &#9664;
-        </button>
-        <div className="carousel">
+      <div className="projects">
+        <div className="carousel-container">
+          <button className="carousel-button left" onClick={handlePrevClick}>
+            &#9664;
+          </button>
           <div className="card-container">
             <a className='card-link' href={project?.repoType}>
               <div className='card-background'>
-                <img className='card-image' src={pfp} alt={project?.title} />
+                <img className='card-image' src={project?.image ?? codelogo} alt={project?.title} />
                 <div>
                   <h3 className="card-title">{project?.title}</h3>
                   <p className="card-subtitle">{project?.date}</p>
@@ -51,13 +54,12 @@ const Projects = ({ translate }) => {
               </div>
             </a>
           </div>
+          <button className="carousel-button right" onClick={handleNextClick}>
+            &#9654;
+          </button>
         </div>
-        <button className="carousel-button right" onClick={handleNextClick}>
-          &#9654;
-        </button>
       </div>
-    </div>
-    </div>
+    </div >
   );
 };
 
